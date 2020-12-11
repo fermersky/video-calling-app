3fa0c631e7b9cd673be4b3a31fb70ec7a7eab5c7
fermersky <forte201253@gmail.com>
2020-12-11 21:56:12 +0200

    client

---
+++ client/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	let name = 'Nastya';
+</script>
+
+<main>
+	<h1>So call me maybe!</h1>
+	<h2>Available users to call:</h2>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		margin: 0 auto;
+	}
+</style>
